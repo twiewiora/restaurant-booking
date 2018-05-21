e5d163da7025dcf14bb7a4655140cbe6760cf0f4
Kevin <kevinpietrzyk@gmail.com>
2018-05-21 23:19:35 +0200

    login and registration frontend

---
+++ src/app/model/reservation.ts
@@ -0,0 +1,12 @@
+export class Rsservation {
+
+  startTime: Date;
+  endTime: Date;
+  comment: string;
+  tableId: number;
+  //client: User;
+
+  constructor() { }
+
+
+}
---
+++ src/app/model/restaurant.ts
@@ -0,0 +1,10 @@
+export class Restaurant {
+
+  id: number;
+  name: string;
+
+
+  constructor() { }
+
+
+}
